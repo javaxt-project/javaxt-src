62bb38296dbf565a7d62d168825e12b0934186b3
pborissow <peter.borissow@yahoo.com>
2022-09-29 09:11:14 -0400

    Added "public" attribute to the Method class

--- src/javaxt/utils/src/Method.java
+++ src/javaxt/utils/src/Method.java
@@ -5,55 +5,64 @@ import javaxt.json.*;
 public class Method implements Member {
     private String name;
     private String description;
-    private boolean isStatic;
+    private boolean isStatic = false;
+    private boolean isPublic = true;
     private String returnType;
     private ArrayList<Parameter> parameters;
-    
+
     public Method(String name){
         this.name = name;
         this.parameters = new ArrayList<>();
     }
-    
+
+    public boolean isPublic(){
+        return isPublic;
+    }
+
+    public void setPublic(boolean isPublic){
+        this.isPublic = isPublic;
+    }
+
     public void setStatic(boolean isStatic){
         this.isStatic = isStatic;
     }
-    
+
     public boolean isStatic(){
         return isStatic;
     }
-    
+
     public void setReturnType(String returnType){
         this.returnType = returnType;
     }
-    
+
     public String getReturnType(){
         return returnType;
     }
-    
+
     public String getName(){
         return name;
     }
-    
+
     public void setDescription(String description){
         this.description = description;
     }
-    
+
     public String getDescription(){
         return description;
     }
-    
+
     public void addParameter(Parameter parameter){
         this.parameters.add(parameter);
     }
-    
+
     public ArrayList<Parameter> getParameters(){
         return parameters;
     }
-    
+
     public String toString(){
         return name;
     }
-    
+
     public JSONObject toJson(){
         JSONObject json = new JSONObject();
         json.set("name", name);
